275addd4a5987867474c37c79c202183ab13e657
isaychris <adaptxsurvive@gmail.com>
2018-05-31 07:27:12 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='seiya-bot',
+    version='',
+    packages=[''],
+    url='https://github.com/isaychris',
+    license='',
+    author='isaychris',
+    author_email='',
+    description='A simple discord bot using discord.py'
+)
